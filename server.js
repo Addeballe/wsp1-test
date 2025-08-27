0447c703197e6f6f6af2423a745695d0afbcaad1
adrian <adrian.nilsson@elev.ga.ntig.se>
2025-08-27 10:17:03 +0200

    setup

---
+++ server.js
@@ -0,0 +1,11 @@
+import Express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(300, () => {
+    console.log("Server is running, Niggas!")
+})
